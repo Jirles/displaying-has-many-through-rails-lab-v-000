4df17973edd4567d868a6cbea90a2fd5b45f2c1d
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 12:54:42 +0000

    automatically backed up by learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,11 +1,11 @@
 class DoctorsController < ApplicationController
 
-  def index 
-    @doctors = Doctor.all 
-  end 
+  def index
+    @doctors = Doctor.all
+  end
 
-  def show 
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
 
-  end 
-  
 end
