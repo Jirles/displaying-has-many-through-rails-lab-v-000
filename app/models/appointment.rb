c0e680148343a3bd6b756859e68e2e29f4a07412
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 12:50:48 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
 
   def readable_datetime
-    self.appointment_datetime.strftime("%B %e, %Y at %k:%M") "January 12, 2016 at 3:00"
+    self.appointment_datetime.strftime("%B %e, %Y at %k:%M") #=>"January 12, 2016 at 3:00"
   end
 end
